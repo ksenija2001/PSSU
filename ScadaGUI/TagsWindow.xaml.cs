c1de815d348142b8bb13fbb2c903c08bd4711fc2
Xenia <barakovicksenija@gmail.com>
2024-06-11 14:07:19 +0100

    Tag creation
    Bug with data disappearing from data grid when a row is clicked multiple times

--- ScadaGUI/TagsWindow.xaml.cs
+++ ScadaGUI/TagsWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Xml.Linq;
 using DataConcentrator;
+using MaterialDesignThemes.Wpf;
 using Microsoft.Xaml.Behaviors.Layout;
 using OxyPlot;
 
@@ -26,20 +27,30 @@ namespace ScadaGUI
     public partial class TagsWindow : Window
     {
         private bool IO {  get; set; }
-        public List<ScanState> StatesList { get; set; }
+        private int LastSelectedD {  get; set; }
+        private int LastSelectedA { get; set; }
+
+        private AlarmsWindow alarms {  get; set; }
+       
+
         public TagsWindow()
         {
             InitializeComponent();
             IO = true;
+            LastSelectedD = -2;
+            LastSelectedA = -2;
 
-            StatesList = Enum.GetValues(typeof(ScanState)).Cast<ScanState>().ToList();
-            States.ItemsSource = StatesList;
+            dataGridDITags.SelectionChanged -= dataGridD_SelectionChanged;
+            dataGridAITags.SelectionChanged -= dataGridA_SelectionChanged;
 
             using (DBModel.IOContext context = new DBModel.IOContext())
             {
-                dataGridDITags.ItemsSource = context.DIs.ToList();
-                dataGridAITags.ItemsSource = context.AIs.ToList();
+                dataGridDITags.ItemsSource = context.Tags.OfType<DBModel.DI>().ToList();
+                dataGridAITags.ItemsSource = context.Tags.OfType<DBModel.AI>().ToList();
             }
+
+            dataGridDITags.SelectionChanged += dataGridD_SelectionChanged;
+            dataGridAITags.SelectionChanged += dataGridA_SelectionChanged;
         }
 
         private void MenuItemViewInputs_Click(object sender, RoutedEventArgs e)
@@ -49,8 +60,8 @@ namespace ScadaGUI
 
             using (DBModel.IOContext context = new DBModel.IOContext())
             {
-                dataGridDITags.ItemsSource = context.DIs.ToList();
-                dataGridAITags.ItemsSource = context.AIs.ToList();
+                dataGridDITags.ItemsSource = context.Tags.OfType<DBModel.DI>().ToList();
+                dataGridAITags.ItemsSource = context.Tags.OfType<DBModel.AI>().ToList();
             }
         }
 
@@ -61,19 +72,45 @@ namespace ScadaGUI
 
             using (DBModel.IOContext context = new DBModel.IOContext())
             {
-                dataGridDITags.ItemsSource = context.DOs.ToList();
-                dataGridAITags.ItemsSource = context.AOs.ToList();
+                dataGridDITags.ItemsSource = context.Tags.OfType<DBModel.DO>().ToList();
+                dataGridAITags.ItemsSource = context.Tags.OfType<DBModel.AO>().ToList();
 
             }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            if (alarms != null && alarms.IsEnabled == true)
+                alarms.Close();
             Application.Current.MainWindow.Show();
+            
         }
 
         private void Window_Activated(object sender, EventArgs e)
         {
+            //dataGridDITags.SelectionChanged -= dataGridD_SelectionChanged;
+            //dataGridAITags.SelectionChanged -= dataGridA_SelectionChanged;
+
+            //if (IO)
+            //{
+            //    using (DBModel.IOContext context = new DBModel.IOContext())
+            //    {
+            //        dataGridDITags.ItemsSource = context.Tags.OfType<DBModel.DI>().ToList();
+            //        dataGridAITags.ItemsSource = context.Tags.OfType<DBModel.AI>().ToList();
+            //    }
+            //}
+            //else
+            //{
+            //    using (DBModel.IOContext context = new DBModel.IOContext())
+            //    {
+            //        dataGridDITags.ItemsSource = context.Tags.OfType<DBModel.DO>().ToList();
+            //        dataGridAITags.ItemsSource = context.Tags.OfType<DBModel.AO>().ToList();
+
+            //    }
+            //}
+
+            //dataGridDITags.SelectionChanged += dataGridD_SelectionChanged;
+            //dataGridAITags.SelectionChanged += dataGridA_SelectionChanged;
 
         }
 
@@ -82,57 +119,121 @@ namespace ScadaGUI
 
         }
 
-        private void dataGridTags_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void dataGridA_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Closes the most current open window
-            for(int i=0; i<Application.Current.Windows.Count-1; i++)
+            var itemA = dataGridAITags.SelectedItem;
+            if (itemA != null && LastSelectedA != dataGridAITags.SelectedIndex)
             {
-                if (Application.Current.Windows[i].GetType() == typeof(AlarmsWindow))
+                LastSelectedA = dataGridAITags.SelectedIndex;
+                if (IO)
                 {
-                    Application.Current.Windows[i].Close();
-                }
-            }
-            // Application.Current.Windows[Application.Current.Windows.Count - 1].Close();
-            if (IO)
-            {
-                // TODO check if there is an item in selected row
-                var itemD = dataGridDITags.SelectedItem;
-                var itemA = dataGridAITags.SelectedItem;
-                AlarmsWindow alarms;
-                if (itemD != null)
-                {
-                    alarms = new AlarmsWindow(itemD);
+                    List<DBModel.Alarm> a = new List<DBModel.Alarm>();
+                    using (DBModel.IOContext context = new DBModel.IOContext())
+                    {
+                        string name = ((DBModel.Tag)itemA).Name;
+                        var temp = context.Tags.Where(n => n.Name == name).FirstOrDefault();
+                        a = ((DBModel.AI)temp).Alarms;
+                    }
+
+                    if (alarms != null && alarms.IsActive == true)
+                        alarms.Close();
+                    alarms = new AlarmsWindow(a);
                     alarms.Show();
                     this.Focus();
                 }
-                else if (itemA != null)
+                else if (alarms != null && alarms.IsActive == true)
+                    alarms.Close();
+            }
+            else if (LastSelectedA != -2 && LastSelectedA != dataGridAITags.SelectedIndex)
+            {
+                var item = dataGridAITags.Items[LastSelectedA];
+                using (DBModel.IOContext context = new DBModel.IOContext())
                 {
-                    alarms = new AlarmsWindow(itemA);
+                    DBTagHandler.Update(context, item);
+                }
+                LastSelectedA = -2;
+            }
+
+        }
+
+        private void dataGridD_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var itemD = dataGridDITags.SelectedItem;
+
+            if (itemD != null && LastSelectedD != dataGridDITags.SelectedIndex)
+            {
+                LastSelectedD = dataGridDITags.SelectedIndex;
+                if (IO)
+                {
+                    List<DBModel.Alarm> a = new List<DBModel.Alarm>();
+                    using (DBModel.IOContext context = new DBModel.IOContext())
+                    {
+                        string name = ((DBModel.Tag)itemD).Name;
+                        var temp = context.Tags.Where(n => n.Name == name).FirstOrDefault();
+                        a = ((DBModel.DI)temp).Alarms;
+                    }
+
+                    if (alarms != null && alarms.IsActive == true)
+                        alarms.Close();
+                    alarms = new AlarmsWindow(a);
                     alarms.Show();
                     this.Focus();
                 }
+                else if (alarms != null && alarms.IsEnabled == true)
+                    alarms.Close();
+            }
+            else if (LastSelectedD != -2 && LastSelectedD != dataGridDITags.SelectedIndex)
+            {
+                //var item = dataGridDITags.Items[LastSelectedD];
+                //using (DBModel.IOContext context = new DBModel.IOContext())
+                //{
+                //    DBTagHandler.Update(context, item);
+                //}
+                //sLastSelectedD = -2;
             }
 
-           
-           
-           
-        }
-
-        private void dataGridDITags_LostFocus(object sender, RoutedEventArgs e)
-        {
-           
-
-        }
-
-        private void dataGridDITags_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
-        {
-            
         }
 
         private void MenuItemCreate_Click(object sender, RoutedEventArgs e)
         {
-            TagDetails tag = new TagDetails();
+            TagDetails tag = new TagDetails("Create");
             tag.ShowDialog();
         }
+
+        private void dataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName == "Connected" || e.PropertyName == "ScanState")
+            {
+                DataGridCheckBoxColumn col = new DataGridCheckBoxColumn();
+                col.Header = e.Column.Header;
+                Binding binding = new Binding(e.PropertyName);
+                col.Binding = binding;
+                col.IsReadOnly = false;
+                e.Column = col;
+            }
+            else if (e.PropertyName == "IOAddress")
+            {
+                System.Windows.Controls.DataGridComboBoxColumn col = new System.Windows.Controls.DataGridComboBoxColumn();
+                col.Header = e.Column.Header;
+                Binding binding = new Binding(e.PropertyName);
+                col.SelectedItemBinding = binding;
+                col.IsReadOnly = false;
+                // TODO Add addresses from PLC
+                col.ItemsSource = new List<string>
+                {
+                    "ADDR001",
+                    "ADDR002",
+                    "ADDR003",
+                    "ADDR004"
+                };
+                e.Column = col;
+            }
+            else if (e.PropertyName == "Alarms")
+            {
+                e.Cancel = true;
+            }
+        }
+
+     
     }
 }
