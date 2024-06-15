b121dd0e5c84a4cc76b3086aaaa682c03cf9f4bf
Xenia <barakovicksenija@gmail.com>
2024-06-15 12:44:54 +0100

    Alarms, events for alarms, new dbmodel

--- ScadaGUI/AllAlarmsWindow.xaml.cs
+++ ScadaGUI/AllAlarmsWindow.xaml.cs
@@ -60,37 +60,6 @@ namespace ScadaGUI
             this.Close();
         }
 
-        private void MenuItemDelete_Click(object sender, RoutedEventArgs e)
-        {
-            var menuItem = (MenuItem)sender;
-            var contextMenu = (ContextMenu)menuItem.Parent;
-            var item = ((DataGrid)contextMenu.PlacementTarget).SelectedCells[0].Item;
-
-            var response = MessageBox.Show($"Do you really want to permanantly delete this alarm?", "Question?", MessageBoxButton.YesNo);
-
-            if (response == MessageBoxResult.Yes)
-            {
-                //using (DBModel.IOContext context = new DBModel.IOContext())
-                //{
-                //    var data = context.Tags.Where(n => n.Name == tagName).FirstOrDefault();
-                //    if (data.GetType() == typeof(DBModel.AI))
-                //    {
-                //        DBModel.Alarm alarm = ((DBModel.AI)data).Alarms.Where(n => n.Id == ((DBModel.Alarm)item).Id).FirstOrDefault();
-                //        ((DBModel.AI)data).Alarms.Remove(alarm);
-                //        DBTagHandler.Update(context, (DBModel.AI)data);
-                //    }
-                //    else
-                //    {
-                //        DBModel.Alarm alarm = ((DBModel.DI)data).Alarms.Where(n => n.Id == ((DBModel.Alarm)item).Id).FirstOrDefault();
-                //        ((DBModel.DI)data).Alarms.Remove(alarm);
-                //        DBTagHandler.Update(context, (DBModel.DI)data);
-                //    }
-                //}
-
-                //RefreshSource();
-            }
-        }
-
         private void Window_Closed(object sender, EventArgs e)
         {
             Application.Current.MainWindow.Show();
