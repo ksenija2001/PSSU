325804ade18a369d4fba32a6d8e10761f6b9b4cb
lea antic <anticlea1@gmail.com>
2024-06-16 17:01:02 +0200

    Plotting and thread fixes
    -Added event when value is changed while scanning and plotting of the value.
    -Fixed crash in thread when deletion is attempted.
    -Fixed creating another thread when PLC is started after entry has been added to the base.

--- DataConcentrator/PLCDataHandler.cs
+++ DataConcentrator/PLCDataHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,12 +13,18 @@ using PLCSimulator;
 using static DataConcentrator.DBModel;
 
 namespace DataConcentrator {
+
+    public delegate void ValueScanned(double val, double time);
+
     public static class PLCDataHandler {
 
         private static PLCSimulatorManager PLCSim;
         public static Dictionary<string, double> PLCData = new Dictionary<string, double>();
         private static List<Thread> ActiveThreads = new List<Thread>();
         public static bool PLCStarted = false;
+        public static event ValueScanned ValueChanged;
+        public static string currently_showing;
+        static readonly Object locker = new Object();
 
         public static void ConnectPLC() {
             PLCSim = new PLCSimulatorManager();
@@ -29,8 +36,6 @@ namespace DataConcentrator {
         }
 
         public static void StartScanner(Tag tag, Type type) {
-
-
             if (type == typeof(DBModel.DI)) {
 
                 DBModel.DI tagDI = (DBModel.DI)tag;
@@ -52,34 +57,50 @@ namespace DataConcentrator {
         // TODO: test alarm logging
         private static void Scanning(string name, double time, string tagAddress, Type type) {
 
-            List<Alarm> alarms;
+            List<Alarm> alarms = new List<Alarm>();
             while (true) {
                 PLCData[tagAddress] = PLCSim.GetDataForAddress(tagAddress);
+                lock (locker) {
+                    if (currently_showing == name) {
+                        ValueChanged(PLCData[tagAddress], time);
+                    }
+                }
                 if (type == typeof(DBModel.AI)) {
 
                     using (var context = new DBModel.IOContext()) {
-                        alarms = context.Tags.OfType<DBModel.AI>().Where(x => x.Name == name).FirstOrDefault().Alarms;
+                        var AIs = context.Tags.OfType<DBModel.AI>().Where(x => x.Name == name).FirstOrDefault();
+                        if (AIs != null) {
+                            alarms = AIs.Alarms;
+                        }
                     }
 
-                    var alarms_above = alarms.FindAll(x => x.Activate == ActiveWhen.ABOVE).Where(x => x.Value > PLCData[tagAddress]);
-                    var alarms_below = alarms.FindAll(x => x.Activate == ActiveWhen.BELOW).Where(x => x.Value < PLCData[tagAddress]);
-                    if (alarms_above.Any()) {
-                        AddAlarm(name, alarms_above);
+                    if (alarms != null) {
+                        var alarms_above = alarms.FindAll(x => x.Activate == ActiveWhen.ABOVE).Where(x => x.Value > PLCData[tagAddress]);
+                        var alarms_below = alarms.FindAll(x => x.Activate == ActiveWhen.BELOW).Where(x => x.Value < PLCData[tagAddress]);
+                        if (alarms_above.Any()) {
+                            AddAlarm(name, alarms_above);
+                        }
+                        if (alarms_below.Any()) {
+                            AddAlarm(name, alarms_below);
+                        }
                     }
-                    if (alarms_below.Any()) {
-                        AddAlarm(name, alarms_below);
-                    }
-
                 }
                 else {
 
                     using (var context = new DBModel.IOContext()) {
-                        alarms = context.Tags.OfType<DBModel.DI>().Where(x => x.Name == name).FirstOrDefault().Alarms;
+                        var DIs = context.Tags.OfType<DBModel.DI>().Where(x => x.Name == name).FirstOrDefault();
+                        if (DIs != null) {
+                            if (DIs != null) {
+                                alarms = DIs.Alarms;
+                            }
+                        }
                     }
 
-                    var alarms_equal = alarms.FindAll(x => x.Activate == ActiveWhen.BELOW).Where(x => x.Value == PLCData[tagAddress]);
-                    if (alarms_equal.Any()) {
-                        AddAlarm(name, alarms_equal);
+                    if (alarms != null) {
+                        var alarms_equal = alarms.FindAll(x => x.Activate == ActiveWhen.BELOW).Where(x => x.Value == PLCData[tagAddress]);
+                        if (alarms_equal.Any()) {
+                            AddAlarm(name, alarms_equal);
+                        }
                     }
                 }
 
--- ScadaGUI/MainWindow.xaml.cs
+++ ScadaGUI/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ using DataConcentrator;
 using System.Xml.Serialization;
 using System.Data.Entity;
 using System.Data.SqlClient;
+using OxyPlot.Wpf;
+using System.Runtime.Remoting.Contexts;
 
 
 namespace ScadaGUI
@@ -28,6 +30,8 @@ namespace ScadaGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        GraphViewModel graph = new GraphViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,8 +40,8 @@ namespace ScadaGUI
             {
                 alarmListView.Items.Clear();
                 alarmListView.ItemsSource = context.LogAlarms.ToList();
+                tagComboBox.ItemsSource = context.Tags.Select(x => x.Name).ToList();
             }
-
         }
 
         private void Window_Activated(object sender, EventArgs e)
@@ -88,6 +92,7 @@ namespace ScadaGUI
             List<DBModel.AI> tagsAI;
             using (var context = new DBModel.IOContext()) {
                 tagsAI = context.Tags.OfType<DBModel.AI>().ToList();
+                tagComboBox.ItemsSource = context.Tags.Select(x => x.Name).ToList();
             }
            
             foreach (var entry in tagsAI) {
@@ -98,20 +103,44 @@ namespace ScadaGUI
 
             PLCDataHandler.PLCStarted = true;
             MessageBox.Show("PLC started successfully");
+            
         }
 
-  
+        private void OnDataChanged(object sender, EventArgs e) {
+            PLCDataHandler.currently_showing = ((ComboBox)sender).Text;
+            graph.ReinitializeList();
+            GraphCtl.InvalidatePlot(true);
+        }
     }
 
+    public class GraphViewModel {
 
-    // TO FIX
-    public class MainViewModel {
-        public MainViewModel() {
-            this.MyModel = new PlotModel { Title = "" };
-            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        public PlotModel GraphDisplay { get; set; }
+        private LineSeries line = new LineSeries();
+        //private List<double> values = new List<double>();
+        private int len = 0;
+        
+
+        public GraphViewModel() {
+            this.GraphDisplay = new PlotModel { Title = "" };
+            PLCDataHandler.ValueChanged += DisplayData;
+            line.Color = OxyColor.Parse("#FFB39DDB");
+            GraphDisplay.Series.Add(line);
         }
 
-        public PlotModel MyModel { get; private set; }
+        private void DisplayData(double val, double scanTime) {
+            line.Points.Add(new DataPoint(len*scanTime, val));
+            GraphDisplay.InvalidatePlot(true);
+            ++len;
+        }
+
+        public void ReinitializeList() {
+            len = 0;
+            line.Points.Clear();
+            //GraphDisplay.Series.Clear();
+            //GraphDisplay.Annotations.Clear();
+            GraphDisplay.InvalidatePlot(true);
+        }
     }
 
    
